8bca3610990b8b9274bab110d2d9ae4882d7ce1c
Aastha Dhangar <aasthaedu.1904@gmail.com>
2025-02-24 14:17:28 +0530

    Add files via upload

---
+++ 123 elements.py
@@ -0,0 +1,6 @@
+my_list = [10,20,30,40,50,60,70,80,90]
+print("enter three elements:", my_list[:3])
+print("Element excluding the first and last:", my_list[1:-1])
+print("last element:", my_list[-1])
+my_list.sort(reverse=True)
+print("list sorted in descending order:", my_list)
